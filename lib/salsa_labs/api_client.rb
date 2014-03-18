28e37e628cff4a80d2a51b8428bda782cc90d6b7
Josh Levinger <josh@levinger.net>
2014-03-17 17:22:07 -0700

    move ApiObjectParameterList to ApiClient class

--- lib/salsa_labs/api_client.rb
+++ lib/salsa_labs/api_client.rb
@@ -105,4 +105,56 @@ module SalsaLabs
     end
   end
 
+
+  ##
+  # Object used to translate an attributes hash to API's expected parameter list
+  # Deals with weird capitalization
+  ##
+  class ApiObjectParameterList
+
+    def initialize(attributes)
+      @attributes = attributes
+      capitalize!
+    end
+
+    def capitalize
+      capitalized_attributes = {}
+
+      @attributes.each do |key, value|
+        #re-capitalize according to Salsa's unique requirements
+
+        #deal with exceptions first
+        if key.end_with? '_key'
+          #asdf_key -> asdf_KEY
+          parts = key.split('_')
+          capitalized_key = [parts[0..-2],parts.last.upcase].join('_')
+        elsif key == 'mi'
+          #middle initial is special case
+          capitalized_key = "MI"
+        elsif key == 'uid'
+          #uid is always lower case
+          capitalized_key = 'uid'
+        elsif key.start_with? 'private'
+          # private_ab_cd_1 -> PRIVATE_Ab_Cd_1
+          parts = key.split('_')
+          last_parts = parts[1..-1].map{|part| part.capitalize}
+          capitalized_key = [parts.first.upcase,last_parts].join('_')
+        else
+          #all others are capitalized normally
+          capitalized_key = (key.split('_').map {|part| part.capitalize}).join('_')
+        end
+
+        capitalized_attributes[capitalized_key] = value
+      end
+
+      capitalized_attributes
+    end
+
+    def capitalize!
+      @attributes = capitalize
+    end
+
+    attr_reader :attributes
+  end
+
 end
--- lib/salsa_labs/salsa_objects_saver.rb
+++ lib/salsa_labs/salsa_objects_saver.rb
@@ -9,7 +9,7 @@ module SalsaLabs
     end
 
     def save(data)
-      parameters = SalsaLabsApiObjectParameterList.new(data)
+      parameters = SalsaLabs::ApiObjectParameterList.new(data)
       api_call(parameters.attributes.update(data))
     end
 
@@ -42,54 +42,6 @@ module SalsaLabs
 
     def parse_response(response)
       Nokogiri::XML(response).css('data')
-    end
-
-    ##
-    # Object used to translate an attributes hash to API's expected parameter list
-    # Deals with weird capitalization
-    ##
-    class SalsaLabsApiObjectParameterList
-
-      def initialize(attributes)
-        @attributes = attributes
-        capitalize
-      end
-
-      def capitalize
-        capitalized_attributes = {}
-
-        @attributes.each do |key, value|
-          #re-capitalize according to Salsa's unique requirements
-
-          #deal with exceptions first
-          if key.end_with? '_key'
-            #asdf_key -> asdf_KEY
-            parts = key.split('_')
-            capitalized_key = [parts[0..-2],parts.last.upcase].join('_')
-          elsif key == 'mi'
-            #middle initial is special case
-            capitalized_key = "MI"
-          elsif key == 'uid'
-            #uid is always lower case
-            capitalized_key = 'uid'
-          elsif key.start_with? 'private'
-            # private_ab_cd_1 -> PRIVATE_Ab_Cd_1
-            parts = key.split('_')
-            last_parts = parts[1..-1].map{|part| part.capitalize}
-            capitalized_key = [parts.first.upcase,last_parts].join('_')
-          else
-            #all others are capitalized normally
-            capitalized_key = (key.split('_').map {|part| part.capitalize}).join('_')
-          end
-
-          capitalized_attributes[capitalized_key] = value
-        end
-
-        capitalized_attributes
-      end
-
-      attr_reader :attributes
-
     end
 
   end
