21edf6ca50e15edb60fd2e4b147765ce2465ab32
Nathan Woodhull <woodhull@gmail.com>
2015-10-30 13:42:35 -0400

    support retrieving supporter action histories.

--- lib/salsa_labs/supporter_action.rb
+++ lib/salsa_labs/supporter_action.rb
@@ -3,24 +3,28 @@ module SalsaLabs
   # Action represents a single instance of a member action in
   # the Salsa Labs CRM.
   ##
-  class Action < SalsaObject
-    integer_attributes :action_key
-    string_attributes :description, :reference_name, :title
+  class SupporterAction < SalsaObject
+    integer_attributes :supporter_action_key, :supporter_key, :action_key, :supporter_action_comment_key
+
+    string_attributes :tracking_code, :salesforce_id, :title
     datetime_attributes :last_modified, :date_created
 
     def self.fetch(filter_parameters = {}, credentials = {})
       ActionsFetcher.new(filter_parameters, credentials).fetch
     end
 
+    def self.object_name
+      'supporter_action'
+    end
   end
 
   ##
   # ActionsFetcher is a service object to pull back a collection of actions from the Salsa Labs API.
   ##
-  class ActionsFetcher < SalsaObjectsFetcher 
+  class SupporterActionsFetcher < SalsaObjectsFetcher
     def initialize(filter_parameters = {}, credentials = {})
       super(filter_parameters, credentials)
-      @object_class = SalsaLabs::Action
+      @object_class = SalsaLabs::SupporterAction
     end
 
     attr_reader :object_parameter
